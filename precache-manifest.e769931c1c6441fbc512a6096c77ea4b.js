17f5be23e8fef8f3514fad9389357309a93ca966
pepper2000 <michael.k.goff@gmail.com>
2020-07-29 15:15:05 -0700

    Updates

---
+++ precache-manifest.e769931c1c6441fbc512a6096c77ea4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ec6702ce312f2b9f9780101af2af2c",
+    "url": "/bt-frontend/index.html"
+  },
+  {
+    "revision": "1ce2141ee1122f12eb7d",
+    "url": "/bt-frontend/static/css/main.14a678a0.chunk.css"
+  },
+  {
+    "revision": "a39a29e882f61d13578e",
+    "url": "/bt-frontend/static/js/2.29dda422.chunk.js"
+  },
+  {
+    "revision": "2560076635a0d32cac6963ae2d9c2d16",
+    "url": "/bt-frontend/static/js/2.29dda422.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ce2141ee1122f12eb7d",
+    "url": "/bt-frontend/static/js/main.40202791.chunk.js"
+  },
+  {
+    "revision": "dbdfc582720a8f650a0e",
+    "url": "/bt-frontend/static/js/runtime-main.953d0c3b.js"
+  }
+]);
